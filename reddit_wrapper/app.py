1e05269d313dc6edac13fa590f8c33d7c247d9f3
MateaLukiccc <ml20200410@student.fon.bg.ac.rs>
2023-12-03 22:23:37 +0100

    Added base for sentiment analysis

--- reddit_wrapper/app.py
+++ reddit_wrapper/app.py
@@ -1,6 +1,6 @@
 import requests
-from json import dumps
 from util import get_token
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
 
 def get_hot_posts(url, header):
@@ -34,10 +34,25 @@ def get_weekly_top_posts(url, header):
             print('permalink: ', post['data']['permalink'])
 
 
+def get_opinion(anime, url, header):
+    sid_obj = SentimentIntensityAnalyzer()
+    payload = {'q': anime, 'restrict_sr': 1, 'type': 'comment', 'sort': 'new'}
+    response = requests.get(url + '/r/anime/search', headers=header, params=payload)
+    values = response.json()
+    count_pos = 0
+    if response.status_code == 200:
+        for comment in values['data']['children']:
+            if sid_obj.polarity_scores(comment['data']['selftext'].replace("\n", " "))['compound'] > 0:
+                count_pos = count_pos + 1
+            else:
+                count_pos = count_pos
+    print(count_pos/len(values['data']['children']))
+
+
 token = get_token()
 base_url = 'https://oauth.reddit.com'
 headers = {'Authorization': token, 'User-Agent': 'APP-NAME by X'}
 
-get_hot_posts(base_url, headers)
+get_opinion('one piece', base_url, headers)
 
 
